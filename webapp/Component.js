b2db4a7c1ef892bc81dcc5b7eacedd9424be6cf2
c5255555 <alisdair.templeton01@sap.com>
2017-09-19 20:53:18 +1000

    Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("vehicle_service_history_load_inquiry.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("vehicle_service_history_load_inquiry.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
